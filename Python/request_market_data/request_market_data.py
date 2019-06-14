9b1d23b39327f9e17953ef2d88bd7372d26e3c58
joris22o <51743304+joris22o@users.noreply.github.com>
2019-06-14 12:48:54 +0200

    Update request_market_data.py

--- Python/request_market_data/request_market_data.py
+++ Python/request_market_data/request_market_data.py
@@ -5,75 +5,68 @@ Copyright (C) 2019 LYNX B.V. All rights reserved.
 # Import ibapi deps
 from ibapi import wrapper
 from ibapi.client import EClient
-from ibapi.utils import iswrapper
 from ibapi.contract import *
-import datetime
+from threading import Thread
+
+from datetime import datetime
+from time import sleep
+
+MARKET_ID = 19004
 
 class Wrapper(wrapper.EWrapper):
     def __init__(self):
         wrapper.EWrapper.__init__(self)
 
+    def tickByTickMidPoint(self, reqId: int, time: int, midPoint: float):
+        """returns tick-by-tick data for tickType = "MidPoint" """
+
+        super().tickByTickMidPoint(reqId, time, midPoint)
+        print("Midpoint. ReqId:", reqId,
+              "Time:", datetime.fromtimestamp(time).strftime("%Y%m%d %H:%M:%S"),
+              "MidPoint:", midPoint)
+
 class Client(EClient):
     def __init__(self, wrapper):
         EClient.__init__(self, wrapper)
 
-class App(Wrapper, Client):
-    def __init__(self):
+    def get_marketData(self, contract, reqId = MARKET_ID):
+
+        # Here we are requesting tickdata for the EUR.GBP Contract.
+        self.reqTickByTickData(reqId, contract, "MidPoint", 0, False)
+
+        MAX_WAITED_SECONDS = 5
+        print("Getting tick data from the server... can take %d second to complete" % MAX_WAITED_SECONDS)
+
+        sleep(MAX_WAITED_SECONDS)
+
+class TestApp(Wrapper, Client):
+    def __init__(self, ipaddress, portid, clientid):
         Wrapper.__init__(self)
         Client.__init__(self, wrapper=self)
-        self.started = False
 
-    @iswrapper
-    def nextValidId(self, orderId: int):
-        super().nextValidId(orderId)
-        self.nextValidOrderId = orderId
-        print("NextValidId:", orderId)
-    
-        # we can start now
-        self.start()
- 
-    @iswrapper
-    def start(self):
-        if self.started:
-            return
+        self.connect(ipaddress, portid, clientid)
 
-        self.started = True
-        print("Executing requests")
+        thread = Thread(target=self.run)
+        thread.start()
 
-        # Define the contract to request
-        contract = Contract()
-        contract.symbol = "EUR"
-        contract.secType = "CASH"
-        contract.currency = "GBP"
-        contract.exchange = "IDEALPRO"
-
-        # Here we are requesting tickdata for the EUR.GBP Contract. The contract's specification is defined above
-        self.reqTickByTickData(19004, contract, "MidPoint", 0, False)
-            
-        print("Executing requests ... finished")
-
-    # Here we print the Midpoint, Request ID and Time returned from the request
-    @iswrapper
-    def tickByTickMidPoint(self, reqId: int, time: int, midPoint: float):
-        super().tickByTickMidPoint(reqId, time, midPoint)
-        print("Midpoint. ReqId:", reqId,
-              "Time:", datetime.datetime.fromtimestamp(time).strftime("%Y%m%d %H:%M:%S"),
-              "MidPoint:", midPoint)
+        setattr(self, "_thread", thread)
 
 def main():
-    try:
-        # Init the App(Wrapper, Client)
-        app = App()
-        # Connect to TWS via socket port 7496
-        app.connect("127.0.0.1", 7496, clientId=0)
-        
-        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
-                                                      app.twsConnectionTime()))
-        # run
-        app.run()
-        
-    except:
-        raise
-        
+    # Init the TestApp(Wrapper, Client)
+    app = TestApp("localhost", 7496, clientid = 0)
+    print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                  app.twsConnectionTime()))
+
+    # Define the contract
+    contract = Contract()
+    contract.symbol = "EUR"
+    contract.secType = "CASH"
+    contract.currency = "USD"
+    contract.exchange = "IDEALPRO"
+    
+    # Here we call the function that includes the request for market data   
+    app.get_marketData(contract)
+
+
 if __name__ == "__main__":
     main()
